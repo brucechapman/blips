b4a4e12c02c2f22450e3253bd42951015c729d22
BruceChapmanNec <bruce.chapman@nec.co.nz>
2017-09-12 20:40:42 +1200

    no need to run read in a process (to make headcons local) because recursive call using `` runs in a new process anyway

--- blips.sh
+++ blips.sh
@@ -1,71 +1,68 @@
 #!/bin/bash
 #
 function create() {
-(
-headcons=
-while true; do
-    if read line;  then
-        if [[ "$line" == '(' ]]; then
-	    next=`create`
-	    while [[ $next != ')' ]] ; do
-	        if [[ $next == EOF ]] ; then
-		    #echo EOF
-		    echo -n '>' >&2
+    headcons=
+    while true; do
+	if read line;  then
+	    if [[ "$line" == '(' ]]; then
+		next=`create`
+		while [[ $next != ')' ]] ; do
+		    if [[ $next == EOF ]] ; then
+			#echo EOF
+			echo -n '>' >&2
+			break
+		    fi
+		    nextcons=`mktemp -u .cons_XXXX`
+		    if [ -z "$headcons" ] ; then
+			headcons=$nextcons
+		    else
+			ln -s ../$nextcons $prevcons/cdr
+		    fi
+		    prevcons=$nextcons
+		    mkdir $nextcons
+		    ln -s ../$next $nextcons/car
+		    next=`create`
+		done
+		if [[ $next == EOF ]] ; then
 		    break
 		fi
-	   	nextcons=`mktemp -u .cons_XXXX`
-		if [ -z "$headcons" ] ; then
-		    headcons=$nextcons
+		if [[ $headcons ]] ; then
+		    ln -s ../nil $nextcons/cdr
+		    echo $headcons
 		else
-		    ln -s ../$nextcons $prevcons/cdr
+		    echo nil
 		fi
-		prevcons=$nextcons
-		mkdir $nextcons
-		ln -s ../$next $nextcons/car
-		next=`create`
-	    done
-	    if [[ $next == EOF ]] ; then
+		break
+	    elif [[ $line == ')' ]] ; then
+		echo $line
+		break
+	    elif echo $line | grep -q -E '^\-?[0-9]+$' ; then
+		int=.int_$line
+		if [ ! -r $int ] ; then
+		    touch $int
+		    echo $line > $int
+		fi
+		echo $int
+		break
+	    elif echo $line | grep -q -E '^"' ; then
+		str=`mktemp -u .str_XXXX`
+		echo $line | sed -e 's/^"//' -e 's/"$//' > $str
+		echo $str
+		break
+	    else
+		# assume a symbol (once we deal with floats)
+		if [ ! -f $line ] ; then
+		    touch $line
+		fi
+		echo $line
 		break
 	    fi
-	    if [[ $headcons ]] ; then
-		ln -s ../nil $nextcons/cdr
-		echo $headcons
-	    else
-		echo nil
-	    fi
-	    break
-	elif [[ $line == ')' ]] ; then
 	    echo $line
-	    break
-	elif echo $line | grep -q -E '^\-?[0-9]+$' ; then
-	    int=.int_$line
-	    if [ ! -r $int ] ; then
-		touch $int
-		echo $line > $int
-	    fi
-	    echo $int
-	    break
-	elif echo $line | grep -q -E '^"' ; then
-	    str=`mktemp -u .str_XXXX`
-	    echo $line | sed -e 's/^"//' -e 's/"$//' > $str
-	    echo $str
-	    break
 	else
-	    # assume a symbol (once we deal with floats)
-	    if [ ! -f $line ] ; then
-		touch $line
-	    fi
-	    echo $line
-            break
-        fi
-        echo $line
-    else
-	echo EOF
-	break 
-    fi
-done
-)
-
+	    echo EOF
+	    break 
+	fi
+    done
 }
 
 
