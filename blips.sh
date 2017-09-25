cb2200c4e5b3b9f0b67a150eebe3f9d4c13e0272
BruceChapmanNec <bruce.chapman@nec.co.nz>
2017-09-25 21:32:00 +1300

    memory in its own directory. A better repl

--- blips.sh
+++ blips.sh
@@ -7,16 +7,46 @@ source blips_print.sh
 source blips_read.sh
 source blips_utils.sh
 
-install_implementations
+SRC_DIR=$(cd $(dirname $0) && pwd)
+CWD_DIR=$(pwd)
+MEM_DIR=$CWD_DIR/.blips_memory
+mkdir -p $MEM_DIR
+echo SRC_DIR=$SRC_DIR
+echo MEM_DIR=$MEM_DIR
+echo CWD_DIR=$CWD_DIR
 
+cd $MEM_DIR
+
+install_implementations
+bind T T
+function rep() {
 expr=`tokenise | create`
-#echo expr=$expr
-result=`eval_impl $expr`
-#set -vx
-#echo result of eval=$result >&2
-echo -n '='
-print $result
-echo ''
+    #echo expr=$expr
+    result=`eval_impl $expr`
+    #set -vx
+    #echo result of eval=$result >&2
+    echo -n '='
+    print $result
+    echo ''
+}
+
+function repl() {
+    while [ true ] ; do
+        #echo -n '?:'
+	read -e -p '?: ' line
+	if [[ $line == '!exit' ]] ; then
+	    break
+	fi
+	expr=$(echo $line | tokenise | create )
+	result=$(eval_impl $expr)
+	echo -n '='
+	print $result
+	echo ''
+    done
+}
+
+repl
+
 
 # close to garbage collector
 # find -L . \( -depth 1 -a -name '.*' \) -prune -o -exec ls -l {} \;
